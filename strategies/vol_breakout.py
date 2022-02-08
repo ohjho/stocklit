b54ebcb918fbcaee09a844b86b1644d899a26f6e
ohjho <jho.yvr+git@gmail.com>
2022-02-08 18:37:24 +0800

    major update for detect_volatility_contraction

--- strategies/vol_breakout.py
+++ strategies/vol_breakout.py
@@ -19,6 +19,44 @@ from toolbox.ta_utils import add_avg_penetration, add_MACD, add_Impulse, \
 # - historically higher than average efficiency_ratio
 # - high short ratio
 
+def get_percentile(x, A):
+    ''' return a percentile of x in the sample space of A
+    Args:
+        x: input value to evaluate agains A
+        A: our sample space to compute percentile against
+    ref: https://stackoverflow.com/a/50863943/14285096
+    '''
+    A = np.sort(A)
+    return np.interp(x, A, np.linspace(0,1, len(A)))
+
+def detect_vol_breakout(df, period:int = 22, threshold:float = 1, ignore_gap = True,
+        do_buy = True, add_entry_price = False, ignore_volume = False
+    ):
+    ''' add two new columns vol_breakout and entry_price to df
+    '''
+    # assert 'impulse' in df.columns
+    threshold *= 1 if do_buy else -1
+    ATR_col = 'ATR_' + str(int(period))
+    df = add_ATR(df, period = period, use_ema = True,  col_name = ATR_col,
+            channel_dict = None, return_TR = False
+            ) if ATR_col not in df.columns else df
+
+    close_diff = (df['Close'] - df['Open']) if ignore_gap else \
+                (df['Close'] - df['Close'].shift())
+
+    # this doesn't seem to work, check 336.hk on 11/9
+    volume_confirms = df['Volume'] > df['Volume'].shift().rolling(period).mean()
+    vol_breakout = close_diff > (df[ATR_col].shift() * threshold) if do_buy else \
+                    close_diff < (df[ATR_col].shift() * threshold)
+    if not ignore_volume:
+        vol_breakout = (vol_breakout & volume_confirms)
+    df['vol_breakout'] = vol_breakout
+
+    if add_entry_price:
+        # df['entry_price'] = (df['Open'] + df['ATR'].shift() * threshold) * vol_breakout
+        df['entry_price'] = df['Close'] * vol_breakout
+    return df
+
 def detect_low_vol_pullback(df, period, price_col = 'Low', col_name = 'LVPB'):
     ''' check if low within period is supported by decreasing volume
     Ref: https://www.investopedia.com/terms/l/low_volume_pullback.asp
@@ -28,27 +66,59 @@ def detect_low_vol_pullback(df, period, price_col = 'Low', col_name = 'LVPB'):
     df[col_name] = is_low & (df['Volume'] < vol_avg)
     return df
 
-def detect_volatility_contraction(df, ma_cascade = [100, 50, 25],
+def detect_volatility_contraction(df, atr_periods = [11,5],
+        period: int = 100, threshold: float = 0.05, normalize= True, use_ema = True,
         col_name = 'VCP', debug = False):
     ''' return a column to indicate volatility contraction using ATR
     Args:
-        ma_cascade: check for moving average being lower in cascading order
+        atr_periods: check for ATRs being lower in cascading order (slowest to fastest)
+        period: look-back period, number of bars
+        threshold: current ATR must be less than this percentile within the look-back period
     '''
-    assert 'ATR' in df.columns, 'add_volatility_contraction: must add ATR first'
-    assert len(ma_cascade) >= 2, 'add_volatility_contraction: must have at least two moving average periods'
-    # DO we need to normalize ATR?
-    l_ma_series = [df['ATR'].rolling(ma).mean().shift()
-                    for ma in ma_cascade]
-    df[col_name] = df['ATR'] < l_ma_series[-1]
+    assert 'ATR' in df.columns, 'detect_volatiliy_contraction: must add ATR first'
+    assert len(atr_periods) >= 2, 'detect_volatility_contraction: must have at least two moving average periods'
+    for p in atr_periods:
+        df = add_ATR(df, period = p, col_name = f'ATR_{p}',
+                use_ema = use_ema, channel_dict = None, normalize = normalize)
+    df['ATR_percentile'] = df['ATR'].rolling(period).apply(
+                            lambda A: get_percentile(x = A[-1], A = A)
+                            )
+    df[col_name] = df['ATR_percentile'] < threshold
 
-    for i in range(len(ma_cascade))[1:]:
-        df[col_name] = df[col_name] & (l_ma_series[i-1] > l_ma_series[i])
+    for i in range(len(atr_periods))[1:]:
+        ATR_slow = f'ATR_{atr_periods[i-1]}'
+        ATR_fast = f'ATR_{atr_periods[i]}'
+        df[col_name] = df[col_name] & (df[ATR_slow] > df[ATR_fast])
 
-    if debug:
-        for ma, ma_series in zip(ma_cascade, l_ma_series):
-            df[f'ATR_{ma}ma'] = ma_series
+    if not debug:
+        for p in atr_periods:
+            del df[f'ATR_{p}']
+        del df['ATR_percentile']
     return df
 
+# def detect_volatility_contraction(df, ma_cascade = [100, 50, 25],
+#         col_name = 'VCP', debug = False):
+#     ''' return a column to indicate volatility contraction using ATR
+#     Args:
+#         ma_cascade: check for moving average being lower in cascading order
+#     '''
+#     assert 'ATR' in df.columns, 'add_volatility_contraction: must add ATR first'
+#     assert len(ma_cascade) >= 2, 'add_volatility_contraction: must have at least two moving average periods'
+#     # DO we need to normalize ATR?
+#     l_ma_series = [df['ATR'].rolling(ma).mean().shift()
+#                     for ma in ma_cascade]
+#     df[col_name] = df['ATR'] < l_ma_series[-1]
+#
+#     for i in range(len(ma_cascade))[1:]:
+#         df[col_name] = df[col_name] & (l_ma_series[i-1] > l_ma_series[i])
+#
+#     if debug:
+#         for ma, ma_series in zip(ma_cascade, l_ma_series):
+#             df[f'ATR_{ma}ma'] = ma_series
+#     return df
+
+### This is for the VCP dtection
+### 
 def detect_VCP(df, ma_cascade = [100,50,25], lvpb_period = 22, ATR_period = 5,
         debug_mode = False, normalize_ATR = False, col_name = 'VCP_setup'):
     '''Minervini's Volatility Contraction Pattern detection
@@ -97,33 +167,6 @@ def detect_VCP(df, ma_cascade = [100,50,25], lvpb_period = 22, ATR_period = 5,
     # # return ((period_return> min_return) & (vol_contracts < vol_contracts_threshold))
     # return ((period_return> min_return) & (atr_slope < vol_contract_threshold))
 
-def detect_vol_breakout(df, period = 22, threshold = 1, ignore_gap = True,
-        do_buy = True, add_entry_price = False, ignore_volume = False
-    ):
-    ''' add two new columns vol_breakout and entry_price to df
-    '''
-    # assert 'impulse' in df.columns
-    threshold *= 1 if do_buy else -1
-    ATR_col = 'ATR_' + str(int(period))
-    df = add_ATR(df, period = period, use_ema = True,  col_name = ATR_col,
-            channel_dict = None, return_TR = False
-            ) if ATR_col not in df.columns else df
-
-    close_diff = (df['Close'] - df['Open']) if ignore_gap else \
-                (df['Close'] - df['Close'].shift())
-
-    # this doesn't seem to work, check 336.hk on 11/9
-    volume_confirms = df['Volume'] > df['Volume'].shift().rolling(period).mean()
-    vol_breakout = close_diff > (df[ATR_col].shift() * threshold) if do_buy else \
-                    close_diff < (df[ATR_col].shift() * threshold)
-    if not ignore_volume:
-        vol_breakout = (vol_breakout & volume_confirms)
-    df['vol_breakout'] = vol_breakout
-
-    if add_entry_price:
-        # df['entry_price'] = (df['Open'] + df['ATR'].shift() * threshold) * vol_breakout
-        df['entry_price'] = df['Close'] * vol_breakout
-    return df
 
 def minervini_vcp(df, atr_period, atr_threshold = 1, ignore_gap = False, ignore_volume = False,
     vcp_params = {'ma_cascade' : [100,50,25],
